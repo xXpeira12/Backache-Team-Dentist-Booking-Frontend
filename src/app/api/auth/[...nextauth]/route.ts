46e90b8cb9eb15f320f36f8641e19389a642afc1
Sirasit Tanajirawat <nongyokkok@gmail.com>
2024-03-26 23:14:13 +0700

    Relocate next auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,45 +1,5 @@
 import NextAuth from "next-auth/next";
-import { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import userLogin from "@/libs/userLogin";
-
-export const authOptions: AuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-              email: { label: "email", type: "email", placeholder: "email" },
-              password: { label: "Password", type: "password", placeholder: "password" }
-            },
-            async authorize(credentials, req) {
-
-              if(!credentials) return null;
-            
-              const user = await userLogin(credentials.email, credentials.password);
-        
-              if (user) {
-                return user
-              } else {
-                  return null
-              }
-            }
-        })
-    ],
-    session: { strategy: 'jwt' },
-    callbacks: {
-        async jwt({token, user}) {
-            return {...token, ...user}
-        },
-        async session({session, token, user}) {
-            session.user = token as any
-            return session
-        }
-    },
-    pages: {
-        signIn: '/signin',
-        signOut: '/signout'
-    }
-}
+import { authOptions } from "@/libs/auth"
 
 const handler = NextAuth(authOptions);
 
